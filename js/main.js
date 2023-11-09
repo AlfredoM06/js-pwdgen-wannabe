04dcecf79dcce39a41067f16ea1ee06b8cf4b64f
AlfredoM06 <Alfredomanfredi06@gmail.com>
2023-11-09 18:01:08 +0100

    added a card

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let name = prompt("Inserisci il tuo nome");
+let surname = prompt("Inserisci il tuo cognome");
+let favoriteColor = prompt("inserisci il tuo colore preferito");
+
+let random = Math.floor(Math.random() * 40);
+
+let password = name + surname + favoriteColor + random;
+
